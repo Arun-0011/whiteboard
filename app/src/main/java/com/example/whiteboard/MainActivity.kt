e006126719beff2b1d4d994e9ba987c9db8296b4
unknown <arunattri499@gmail.com>
2023-04-04 17:37:53 +0530

    Draw line added

--- app/src/main/java/com/example/whiteboard/MainActivity.kt
+++ app/src/main/java/com/example/whiteboard/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.whiteboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.whiteboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    private var isArrowIconClicked = false
+
+    companion object {
+        var path = Path()
+        var paintBrush = Paint()
+        var colorList = ArrayList<Int>()
+        var currentBrush = Color.BLACK
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        supportActionBar?.hide()
+
+        binding?.apply {
+            btnArrow.setOnClickListener {
+                isArrowIconClicked = !isArrowIconClicked
+
+                if (isArrowIconClicked) {
+                    btnArrow.setImageResource(R.drawable.ic_selected_line)
+                    btnArrow.setBackgroundResource(R.drawable.background_cards)
+
+
+                    drawLine.visibility = View.VISIBLE
+                } else {
+                    btnArrow.setImageResource(R.drawable.ic_unselected_line)
+                    btnArrow.setBackgroundResource(R.drawable.background_card)
+                }
+            }
+        }
     }
+
+    private fun currentColor(color: Int) {
+        currentBrush = color
+        path = Path()
+    }
+
 }
---
+++ app/src/main/java/com/example/whiteboard/cons/Line.kt
@@ -0,0 +1,9 @@
+package com.example.whiteboard.cons
+
+data class Line(
+    var startX: Float = 0f,
+    var startY: Float = 0f,
+    var stopX: Float = 0f,
+    var stopY: Float = 0f,
+    var color: Int = 0
+)
